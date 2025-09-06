0ab976f4efe97f110919880d6d1e9d7307484701
anthony <ayomideobaroovooboh@gmail.com>
2025-09-06 18:24:40 +0100

    dto setup

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsNotEmpty()
+  title: string;
+
+  @IsOptional()
+  description?: string;
+}
