db7e525bc8abe86b4e1fd9671d0b40ac0bb873d5
anthony <ayomideobaroovooboh@gmail.com>
2025-09-08 01:59:07 +0100

    connected postgres with prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma.service'
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [TasksModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
