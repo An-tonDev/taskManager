e5ca3c7e5d82e075a161644bf06c1371cd91d1d3
anthony <ayomideobaroovooboh@gmail.com>
2025-09-09 23:42:07 +0100

    JWT G&S

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { prismaService } from './prisma/prisma.service'
+import { prismaService } from './prisma/prisma.service';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule, AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    TasksModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [prismaService],
 })
