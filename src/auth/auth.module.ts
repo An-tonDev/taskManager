23f939554c2d1474cd4ea056e5a5da66c1e39d29
anthony <ayomideobaroovooboh@gmail.com>
2025-09-10 23:30:16 +0100

    AuthM

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,17 +3,23 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { prismaService } from 'src/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersService } from 'src/users/users.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => ({
+        secret: ConfigService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
   ],
-  providers: [AuthService, prismaService],
+  providers: [AuthService, prismaService, JwtStrategy, UsersService],
   controllers: [AuthController],
 })
 export class AuthModule {}
